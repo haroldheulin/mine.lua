1c10c6c518f6fef9add5a4511661ad74e62ef951
HaroldHeulin <harold.heulin@icloud.com>
2026-01-31 18:38:42 +0100

    Add files via upload

--- mine.lua
+++ mine.lua
@@ -1,33 +1,43 @@
-distance = 10
+local distance = 10
 
-function avancer()
-    for d = 1, distance do
-        turtle.forward()
+function avancer(n)
+    for i = 1, n do
         turtle.dig()
-    end
-end
-
-function forward()
-    while distance > 1 do
-        for c = 1, 3 do
-           avancer()
-           turtle.turnRight()
-        end
-        distance = distance - 1
-    end
-end
-
-while true do
-    forward()
-    for x = 1, 6 do
         turtle.forward()
     end
-    tutle.turnRight()
-    for x = 1, 5 do
+end
+
+function creuserCarre(taille)
+    local d = taille
+    while d > 0 do
+        for side = 1, 4 do
+            avancer(d - 1)
+            turtle.turnRight()
+        end
+        d = d - 2
+        if d > 0 then
+            turtle.forward()
+            turtle.turnRight()
+        end
+    end
+end
+
+function retourCoinDepart(taille)
+    turtle.turnRight()
+    turtle.turnRight()
+    for i = 1, taille - 1 do
         turtle.forward()
     end
     turtle.turnRight()
+    for i = 1, taille - 1 do
+        turtle.forward()
+    end
+    turtle.turnRight()
+end
+
+while true do
+    creuserCarre(distance)
+    retourCoinDepart(distance)
     turtle.digDown()
     turtle.down()
-    distance = 10
 end
