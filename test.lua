d198af9de6f8966f833b5beb9e15ccf9e01e2963
HaroldHeulin <harold.heulin@icloud.com>
2026-02-01 13:04:33 +0100

    Add files via upload

---
+++ test.lua
@@ -0,0 +1,16 @@
+print("Finished mining cube")
+turtle.turnLeft()
+turtle.turnLeft()
+for returnX = 1, 5 do
+    turtle.forward()
+end
+turtle.turnRight()
+for returnY = 1, 5 do
+        turtle.forward()
+end
+turtle.turnRight()
+turtle.digDown()
+turtle.down()
+distance = 10
+x = 0
+y = 0
