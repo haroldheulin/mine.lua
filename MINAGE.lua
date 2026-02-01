24205baf8ac6a35d3b2698388db3f8c802b5558f
HaroldHeulin <harold.heulin@icloud.com>
2026-02-01 14:05:54 +0100

    Add files via upload

---
+++ MINAGE.lua
@@ -0,0 +1,68 @@
+distance = 10
+x = 0
+y = 0
+orientation = 1
+
+for couche = 1, 5 do
+    for i = 1, 9 do
+        for c = 1, 4 do
+            for b = 1, distance do
+                turtle.dig()
+                turtle.forward()
+                if orientation == 1 then
+                    x = x + 1
+                elseif orientation == 2 then
+                    y = y + 1
+                elseif orientation == 3 then
+                    x = x - 1
+                elseif orientation == 4 then
+                    y = y - 1
+                end
+                print("X: " .. x .. " Y: " .. y)
+            end
+            turtle.turnRight()
+            print("Orientation start " .. orientation)
+            orientation = orientation + 1
+            print("Orientation end " .. orientation)
+            if orientation > 4 then
+                orientation = 1
+            end
+        end
+        turtle.forward()
+        turtle.turnRight()
+        turtle.dig()
+        turtle.forward()
+        turtle.turnLeft()
+        turtle.dig()
+        distance = distance - 2
+        print(distance)
+    end
+
+    print("Finished mining cube")
+
+    turtle.turnLeft()
+    turtle.turnLeft()
+    for returnX = 1, 5 do
+        turtle.forward()
+    end
+    turtle.turnRight()
+    for returnY = 1, 5 do
+            turtle.forward()
+    end
+    turtle.turnRight()
+    turtle.turnLeft()
+    for i = 1, 4 do
+        turtle.forward()
+    end
+    turtle.turnLeft()
+    for i = 1, 4 do
+        turtle.forward()
+    end
+    turtle.turnRight()
+    turtle.turnRight()
+    turtle.digDown()
+    turtle.down()
+    distance = 10
+    x = 0
+    y = 0
+end
